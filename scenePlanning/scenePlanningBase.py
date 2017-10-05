ea5d8cdbb34c33da88deb03e9c22f3ba0c7a1671
Max Harms <raelifin@gmail.com>
2017-10-04 19:27:08 -0700

    Linted again

--- scenePlanning/scenePlanningBase.py
+++ scenePlanning/scenePlanningBase.py
@@ -3,21 +3,21 @@
 # prepare to receive yes/no resposes from the user
 affirmativeResponses = ['yes', 'y', 'yep', 'affirmative']
 negativeResponses = ['no', 'n', 'nope', 'negative']
-def elicitYesOrNoResponse(): # forces yes/no response from user
+def elicitYesOrNoResponse():  # forces yes/no response from user
     x = input()
     if x.lower() in affirmativeResponses:
         return "yes"
     elif x.lower() in negativeResponses:
-         return "no"
+        return "no"
     else:
-         print('Please respond "yes" or "no".')
-         return elicitYesOrNoResponse()
+        print('Please respond "yes" or "no".')
+        return elicitYesOrNoResponse()
 
 # collect the major structural info for the scene
 POVCharacter = input("What's the name of the point-of-view character?\n")
 goal = input('What is ' + POVCharacter + "'s goal during this scene?\n")
 
-def getObstacle(): # learn the adversary or obstacle name
+def getObstacle():  # learn the adversary or obstacle name
     print("Will another person get in " + POVCharacter + "'s way?")
     if elicitYesOrNoResponse() == "yes":
         print("Who will it be?")
